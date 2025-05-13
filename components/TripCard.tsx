fc366139273eab369285f2246b993c91c345cdbb
DikshaExternLabs <dikshadishu0608@gmail.com>
2025-05-13 23:26:11 +0200

    appwrite setup

--- components/TripCard.tsx
+++ components/TripCard.tsx
@@ -1,6 +1,8 @@
 // @ts-nocheck
 import React from 'react';
 import { Link, useLocation } from 'react-router';
+import { cn, getFirstWord } from '~/lib/utils';
+import { ChipListComponent, ChipsDirective,ChipDirective } from '@syncfusion/ej2-react-buttons';
 
 const TripCard = ({id,name,tags,imageUrl,location,price}:TripCardProps) => {
   
@@ -20,7 +22,21 @@ const TripCard = ({id,name,tags,imageUrl,location,price}:TripCardProps) => {
       <img src='/assets/icons/location-mark.svg' alt='location' className='size-4'/>
       <figcaption>{location}</figcaption>
     </figure>
-    <div className="mt-5 pl=[18px] pr-3.8 pb-5"></div>
+    <div className="mt-5 pl=[18px] pr-3.8 pb-5">
+      <ChipListComponent id="travel-chip" >
+        <ChipsDirective>
+          {tags.map((tag,index) => (
+            <ChipDirective key={index}
+                           text={getFirstWord(tag)}
+                           cssClass={cn( index === 1 ?
+                             '!bg-pink-50 !text-pink-500)' : 'bg-success-50 !text-success-700')}
+            />
+          ))}
+        </ChipsDirective>
+      </ChipListComponent>
+    </div>
+
+      <article className="tripCard-pill" >{price}</article>
     </Link>
   );
 };
