b60f105925ca2c2586641ba3003c307db03a03de
DikshaExternLabs <dikshadishu0608@gmail.com>
2025-05-17 16:24:41 +0200

    worked on user table

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,16 +1,65 @@
 import React from 'react';
 import { Header } from '../../../components';
-import { GridComponent } from '@syncfusion/ej2-react-grids';
+import { ColumnDirective, ColumnsDirective, GridComponent } from '@syncfusion/ej2-react-grids';
+import { users } from '~/constants';
+import { cn } from '~/lib/utils';
 
 const AllUsers = () => {
   return (
     <main className="all-users">
       <Header
         title="manage users"
-        description="filter , sort and access detailed users profile"      />
-      <GridComponent>
+        description="filter , sort and access detailed users profile" />
+      <GridComponent dataSource={users} gridLines="None" >
+        <ColumnsDirective>
+          <ColumnDirective
+          field="name"
+          headerText="Name"
+          width="200"
+          textAlign="Left"
+          template={(props:UserData) => (
+            <div className="flex items-center gap-1.5 px-4" >
+              <img src={props.imageUrl} alt="user" className="rounded-full size-8 expect-square" />
+              <span>{props.name}</span>
+            </div>
+          )}
+          />
+          <ColumnDirective
+          field="email"
+          headerText="Email"
+          width="150"
+          textAlign="Left"
+          />
+          <ColumnDirective
+            field="dateJoined"
+            headerText="Date Joined"
+            width="120"
+            textAlign="Left"
+          />
+          <ColumnDirective
+            field="itineraryCreated"
+            headerText="Trip Created"
+            width="130"
+            textAlign="Left"
+          />
+          <ColumnDirective
+            field="status"
+            headerText="Type"
+            width="100"
+            textAlign="Left"
+            template={({ status }:UserData) => (
+              <article className={cn('status-column', status === 'user' ? 'bg-success-50' : 'bg-gray-50')}>
+                <div className={cn('size-1.5 rounded-full', status === 'user' ? 'bg-success-500' : 'bg-gray-500')}/>
+                  <h3 className={cn('font-inter text-xs font-medium ', status === 'user' ? 'text-success-500' : 'text-gray-500')} >
+                    {status}
+                  </h3>
 
+              </article>
+            )}
+          />
+        </ColumnsDirective>
       </GridComponent>
     </main>
   );
-export default AllUsers;
+}
+export default AllUsers
